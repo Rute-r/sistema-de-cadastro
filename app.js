621d0ac83fadf779e0f8433ab05e5f72a8454530
Rute-r <rute_ke@hotmail.com>
2023-01-24 14:15:16 -0300

    adicionando um console.log

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-console.log("Rodando o sistema de gerenciamento de jogos")
+console.log('Rodando o sistema de gerenciamento de jogos');
+console.log('aplicacao esta se conectando ao banco de dados');
+
+console.log('Enviando para o GitHub');
